41951aea94de8aeadedea8b3d081de532a033dfa
jayden2013 <jay-weav@hotmail.com>
2018-01-13 22:41:16 -0700

    Increased Minimum SDK Version, Getting Project Ready For Image Analysis.

--- app/src/main/java/com/sublux/jayden/sublux/results.java
+++ app/src/main/java/com/sublux/jayden/sublux/results.java
@@ -1,13 +1,38 @@
 package com.sublux.jayden.sublux;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class results extends AppCompatActivity {
+
+    String imageURI = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Intent i = getIntent();
+        //System.out.println(i.getStringExtra("imageURI")); //Testing
+        this.imageURI = i.getStringExtra("imageURI"); //Set Image URI
+
+        //Temporary Bubble Text
+        Toast toast = Toast.makeText(this, imageURI, Toast.LENGTH_LONG);
+        toast.show(); //Show Bubble Text
     }
+
+
+    /**
+     * Method to Analyze an image, probably pixel by pixel. We shall see.
+     */
+    public void analyzeImage(){
+
+
+    }
+
+
+
+
+
 }
